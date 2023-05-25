2974021c070a9cec916d3472c73ee03026e1899a
takase-masayoshi <justice122813@gmail.com>
2023-05-25 17:50:56 +0900

    light

--- DoraFes/Assets/Scenes/MainGame/LazerLight/LazerLight.cs
+++ DoraFes/Assets/Scenes/MainGame/LazerLight/LazerLight.cs
@@ -43,7 +43,7 @@ public class LazerLight : MonoBehaviour
         if (isFever)
         {
             //アクティブにする
-
+            gameObject.SetActive(true);
 
             //毎フレームごとの回転量を計算
             float rotationAmount = rotationSpeed * Time.deltaTime;
@@ -51,32 +51,34 @@ public class LazerLight : MonoBehaviour
             // 現在の回転からZ軸の角度を取得し、初期の角度との差を求める
             float currentZAngle = GetZAngle(transform.rotation);
             float zAngleDifference = GetShortestAngleDifference(initialZAngle, currentZAngle);
+            float a = Mathf.Abs(zAngleDifference);
 
             //上向き回転の場合
             if (rotateUp)
             {
                 //オブジェクトを回転させる
-                transform.Rotate(Vector3.forward, rotationAmount); //z軸回転
+                transform.Rotate(Vector3.forward, -rotationAmount); //z軸回転
                                                                    //transform.Rotate(Vector3.up, rotationAmount);      y軸回転
 
                 //上向き回転が60度以上になったらフラグを切り替える
-                if (zAngleDifference >= upLimit)
+                if (a >= upLimit)
                     rotateUp = false;
             }
             //下向き回転の場合
             else
             {
                 // オブジェクトを回転させる
-                transform.Rotate(Vector3.forward, -rotationAmount);
+                transform.Rotate(Vector3.forward, rotationAmount);
 
                 // 下向き回転が60度以下になったらフラグを切り替える
-                if (-zAngleDifference >= -downLimit)
+                if (-a >= -downLimit)
                     rotateUp = true;
             }
         }
         else
         {
             //非アクティブにする
+            gameObject.SetActive(false);
         }
         
 
