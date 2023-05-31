25a0bd923f3307af3c52e8de30280db08bd5fabd
takase-masayoshi <justice122813@gmail.com>
2023-05-31 14:28:20 +0900

    addCar

--- DoraFes/Assets/Scenes/MainGame/World5/CarSet/MovementCar.cs
+++ DoraFes/Assets/Scenes/MainGame/World5/CarSet/MovementCar.cs
@@ -4,50 +4,68 @@ using UnityEngine;
 
 public class MovementCar : MonoBehaviour
 {
-    public float moveDistance = 10f;  // 移動する距離
-    public float moveSpeed = 5f;  // 移動速度
+    [Header("リポップ座標")]
+    [SerializeField] private float repopPosition;
+
+    [Header("エンド座標")]
+    [SerializeField] private float endPosition;
+
+    [Header("スピード")]
+    [SerializeField] private float speed = 10.0f;
+
+    [Header("右に進むならチェック")]
+    [SerializeField] private bool moveRight;
+
+    private Rigidbody rb;
 
     private Vector3 initialPosition;  // 元の位置
-    private bool isMoving;  // 移動中かどうかのフラグ
-    private float movedDistance = 0f;  // 移動した距離の累計
+
+    private float timeSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         initialPosition = transform.position;
-        isMoving = true;
+        initialPosition.x = repopPosition;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (isMoving)
+        // 移動方向を指定（例えば、右方向に移動）
+        Vector3 movement = new Vector3(1f, 0f, 0f);
+
+
+        // 移動方向に速度を乗算
+        movement *= speed;
+
+        if (moveRight)
         {
-            // x方向に移動する
-            float moveStep = moveSpeed * Time.deltaTime;
-            transform.Translate(Vector3.forward * moveStep);
-
-            // 移動した距離を更新
-            movedDistance += Mathf.Abs(moveStep);
-            
-
-            // 移動した距離が一定値を超えたら元の位置に戻す
-            if (movedDistance >= moveDistance)
-            {
-                //isMoving = false;
-                Invoke("MoveBack", 0.1f);  // 1秒後に元の位置に戻す処理を呼び出す（任意の待ち時間）
-            }
+            // Rigidbodyのvelocityプロパティを設定
+            rb.velocity = movement;
         }
+        else
+        {
+            // Rigidbodyのvelocityプロパティを設定
+            rb.velocity = -movement;
+        }
+        
+
+        // 移動した距離が一定値を超えたら元の位置に戻す
+        if (transform.position.x < endPosition)
+        {
+            //isMoving = false;
+            Invoke("MoveBack", 0.1f);  // 1秒後に元の位置に戻す処理を呼び出す（任意の待ち時間）
+        }
+
     }
     private void MoveBack()
     {
         // 元の位置に戻す
         transform.position = initialPosition;
-        movedDistance = 0f;  // 移動した距離をリセット
-    }
-
-    public void StartMoving()
-    {
-        isMoving = true;
+       
     }
 }
